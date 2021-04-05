a4c8e3e2abb09c258a6b56265df865d1e73591d0
YuJinJu <60469783+YuJinJu@users.noreply.github.com>
2021-04-06 01:06:53 +0900

    success

---
+++ src/level1/포켓몬.java
@@ -0,0 +1,33 @@
+package level1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 포켓몬 {
+    public static int solution(int [] nums) {
+        int answer = 0;
+
+        //중복제거
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i : nums){
+            if(!list.contains(i)) list.add(i);
+        }
+
+        if(list.size() >= nums.length/2) answer = nums.length/2;
+        else answer = list.size();
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int [] n1 = {3,1,2,3};
+        int [] n2 = {3,3,3,2,2,4};
+        int [] n3 = {3,3,3,2,2,2};
+
+        //2 3 2
+        System.out.println(solution(n1));
+        System.out.println(solution(n2));
+        System.out.println(solution(n3));
+    }
+}
