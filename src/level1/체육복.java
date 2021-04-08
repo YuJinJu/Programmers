6cee3eb61e649aaaf2003b6cb7342cf49565d751
YuJinJu <60469783+YuJinJu@users.noreply.github.com>
2021-04-08 17:37:24 +0900

    main 테스트케이스 추가

--- src/level1/체육복.java
+++ src/level1/체육복.java
@@ -47,6 +47,22 @@ public class 체육복 {
 
     public static void main(String[] args) {
 
+        int [] lost = {2,4};
+        int [] reserve = {1,3,5};
+        System.out.println(solution(5,lost,reserve)); //5
+
+        int [] lost1 = {1,2,3};
+        int [] reserve1 = {2,3,4};
+        System.out.println(solution(5,lost1,reserve1));//4
+
+        int [] lost2 = {2,3,4};
+        int [] reserve2 = {1,2,3};
+        System.out.println(solution(5,lost2,reserve2));//4
+
+        int [] lost3 = {3};
+        int [] reserve3 = {1};
+        System.out.println(solution(3,lost3,reserve3));//2
+
     }
 
 }
